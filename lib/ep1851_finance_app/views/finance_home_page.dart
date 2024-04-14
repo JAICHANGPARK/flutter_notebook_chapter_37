4b3fba62ef04f76f9cae6cad65f135641d890234
JAICHANGPARK <aristojeff@gmail.com>
2024-04-14 10:50:00 +0900

    Create finance_home_page.dart

---
+++ lib/ep1851_finance_app/views/finance_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FinanceHomePage extends StatefulWidget {
+  const FinanceHomePage({super.key});
+
+  @override
+  State<FinanceHomePage> createState() => _FinanceHomePageState();
+}
+
+class _FinanceHomePageState extends State<FinanceHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
