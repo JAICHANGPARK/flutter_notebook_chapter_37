3c0d1638e938f44b6bdb3889a71689b9e81fad97
JAICHANGPARK <aristojeff@gmail.com>
2024-04-14 10:50:12 +0900

    Update ep1851_main.dart

--- lib/ep1851_finance_app/ep1851_main.dart
+++ lib/ep1851_finance_app/ep1851_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'views/finance_home_page.dart';
+
 void main() {
   runApp(FinanceApp());
 }
@@ -10,7 +12,7 @@ class FinanceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: FinanceHomePage(),
     );
   }
 }
