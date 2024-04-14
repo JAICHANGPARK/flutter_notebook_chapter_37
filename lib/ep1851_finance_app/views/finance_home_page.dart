7641478462b4212c4be3a47e815a75fda954b889
JAICHANGPARK <aristojeff@gmail.com>
2024-04-14 10:53:25 +0900

    Update finance_home_page.dart

--- lib/ep1851_finance_app/views/finance_home_page.dart
+++ lib/ep1851_finance_app/views/finance_home_page.dart
@@ -11,6 +11,15 @@ class FinanceHomePage extends StatefulWidget {
 class _FinanceHomePageState extends State<FinanceHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
   }
 }
