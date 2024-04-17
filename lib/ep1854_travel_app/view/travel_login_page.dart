9960beaeab2632b063ca87d01ebffe65c576b9ca
JAICHANGPARK <aristojeff@gmail.com>
2024-04-17 21:37:47 +0900

    Update travel_login_page.dart

---
+++ lib/ep1854_travel_app/view/travel_login_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class TravelLoginPage extends StatefulWidget {
+  const TravelLoginPage({super.key});
+
+  @override
+  State<TravelLoginPage> createState() => _TravelLoginPageState();
+}
+
+class _TravelLoginPageState extends State<TravelLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
