8925c8b7e3dd9c66d79125f6260a49815f7f142a
JAICHANGPARK <aristojeff@gmail.com>
2024-04-18 17:07:29 +0900

    Create travel_main_page.dart

---
+++ lib/ep1854_travel_app/view/travel_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TravelMainPage extends StatefulWidget {
+  const TravelMainPage({super.key});
+
+  @override
+  State<TravelMainPage> createState() => _TravelMainPageState();
+}
+
+class _TravelMainPageState extends State<TravelMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
