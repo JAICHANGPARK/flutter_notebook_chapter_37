c9eba758185992920b047f53e4e426fefb3b7ad8
JAICHANGPARK <aristojeff@gmail.com>
2024-04-21 12:36:54 +0900

    Update travel_detail_page.dart

--- lib/ep1854_travel_app/view/travel_detail_page.dart
+++ lib/ep1854_travel_app/view/travel_detail_page.dart
@@ -77,11 +77,13 @@ class _TravelDetailPageState extends State<TravelDetailPage> {
                             )
                           ],
                         ),
+                        Gap(16),
                         SizedBox(
                           height: 400,
-                          child: TabBarView(
-
-                              children: [
+                          child: TabBarView(children: [
+                            Text(
+                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                            ),
                             Text(
                               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                             ),
