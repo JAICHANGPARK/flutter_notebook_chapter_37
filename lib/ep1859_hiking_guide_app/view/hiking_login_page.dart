3e316f62f4165bdb447ed1110ef958800cb19283
JAICHANGPARK <aristojeff@gmail.com>
2024-04-22 17:14:13 +0900

    Create hiking_login_page.dart

---
+++ lib/ep1859_hiking_guide_app/view/hiking_login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HikingLoginPage extends StatelessWidget {
+  const HikingLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
