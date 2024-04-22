dcd97ee1091ca1de0825cf46633e222e27769503
JAICHANGPARK <aristojeff@gmail.com>
2024-04-22 17:27:02 +0900

    Update hiking_login_page.dart

--- lib/ep1859_hiking_guide_app/view/hiking_login_page.dart
+++ lib/ep1859_hiking_guide_app/view/hiking_login_page.dart
@@ -9,8 +9,9 @@ class HikingLoginPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(child: Container()),
+          Expanded(flex: 7, child: Container()),
           Expanded(
+            flex: 6,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
@@ -56,6 +57,7 @@ class HikingLoginPage extends StatelessWidget {
                       child: Text("Sign in with Apple"),
                     ),
                   ),
+                  Gap(8),
                   Text(
                     "Don't you have an account? Register",
                   )
