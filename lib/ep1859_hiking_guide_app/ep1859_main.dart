7c8408ae87d7ff4a3a6803af27cb492e8004f0e4
JAICHANGPARK <aristojeff@gmail.com>
2024-04-22 17:12:14 +0900

    Update ep1859_main.dart

--- lib/ep1859_hiking_guide_app/ep1859_main.dart
+++ lib/ep1859_hiking_guide_app/ep1859_main.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+
+}
+
+class HikingGuideApp extends StatelessWidget {
+  const HikingGuideApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
