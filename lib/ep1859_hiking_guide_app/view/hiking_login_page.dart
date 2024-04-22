d2a3ec472a3acf74334460ff22a57a380363e56c
JAICHANGPARK <aristojeff@gmail.com>
2024-04-22 17:19:11 +0900

    Update hiking_login_page.dart

--- lib/ep1859_hiking_guide_app/view/hiking_login_page.dart
+++ lib/ep1859_hiking_guide_app/view/hiking_login_page.dart
@@ -9,12 +9,27 @@ class HikingLoginPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(child: Container()),
-          Expanded(child: Column(
+          Expanded(
+              child: Column(
             children: [
-
+              Text(
+                "Embark on Your Next Adventure with Us!",
+              ),
+              Text(
+                "Let HikingG be your trusted companion as you explore the great outdoors!",
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[200]!,
+                  ),
+                ),
+                child: Center(
+                  child: Text("Sign in with Google"),
+                ),
+              )
             ],
           )),
-
         ],
       ),
     );
