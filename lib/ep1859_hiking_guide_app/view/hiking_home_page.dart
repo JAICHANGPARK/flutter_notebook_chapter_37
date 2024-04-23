b9bd265a1a6a757d6d9a2249321d35f161fe48c7
JAICHANGPARK <aristojeff@gmail.com>
2024-04-23 20:29:56 +0900

    Create hiking_home_page.dart

---
+++ lib/ep1859_hiking_guide_app/view/hiking_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HikingHomePage extends StatefulWidget {
+  const HikingHomePage({super.key});
+
+  @override
+  State<HikingHomePage> createState() => _HikingHomePageState();
+}
+
+class _HikingHomePageState extends State<HikingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
