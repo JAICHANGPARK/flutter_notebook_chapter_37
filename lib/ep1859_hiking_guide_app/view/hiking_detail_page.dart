73af095b9b6816a12f4b0ee16b62344ac8ef7fe6
JAICHANGPARK <aristojeff@gmail.com>
2024-04-26 17:02:39 +0900

    Update hiking_detail_page.dart

--- lib/ep1859_hiking_guide_app/view/hiking_detail_page.dart
+++ lib/ep1859_hiking_guide_app/view/hiking_detail_page.dart
@@ -38,30 +38,33 @@ class _HikingDetailPageState extends State<HikingDetailPage> {
           ),
           Expanded(
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Mount via Tretes"),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.golf_course,
-                        size: 16,
-                      ),
-                      Text("East Java, Indonesia"),
-                    ],
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200]!,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Mount via Tretes"),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.golf_course,
+                          size: 16,
+                        ),
+                        Text("East Java, Indonesia"),
+                      ],
                     ),
-                  ),
-                  Text("Description"),
-                  Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    maxLines: 3,
-                  ),
-                ],
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200]!,
+                      ),
+                    ),
+                    Text("Description"),
+                    Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                      maxLines: 3,
+                    ),
+                  ],
+                ),
               ),
             ),
           )
