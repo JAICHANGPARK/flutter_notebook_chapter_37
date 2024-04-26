cbac08d0e17f758ee03dc5dccc8bc5a8283924c1
JAICHANGPARK <aristojeff@gmail.com>
2024-04-26 16:52:15 +0900

    Create hiking_detail_page.dart

---
+++ lib/ep1859_hiking_guide_app/view/hiking_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HikingDetailPage extends StatefulWidget {
+  const HikingDetailPage({super.key});
+
+  @override
+  State<HikingDetailPage> createState() => _HikingDetailPageState();
+}
+
+class _HikingDetailPageState extends State<HikingDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
