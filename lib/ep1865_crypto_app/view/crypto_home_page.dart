9ae31a73d336cadde007e21e81f02031b142edd4
JAICHANGPARK <aristojeff@gmail.com>
2024-04-28 10:47:16 +0900

    Update crypto_home_page.dart

--- lib/ep1865_crypto_app/view/crypto_home_page.dart
+++ lib/ep1865_crypto_app/view/crypto_home_page.dart
@@ -16,36 +16,33 @@ class _CryptoHomePageState extends State<CryptoHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Stack(
-                    children: [
-                      Positioned(
-                        left: 40,
-                        child: CircleAvatar(
-                          radius: 26,
-                          backgroundColor: Colors.white,
-                          child: Icon(Icons.add),
-                        ),
+            Row(
+              children: [
+                Expanded(
+                    child: Stack(
+                  children: [
+                    Positioned(
+                      left: 40,
+                      child: CircleAvatar(
+                        radius: 26,
+                        backgroundColor: Colors.white,
+                        child: Icon(Icons.add),
                       ),
-                      Positioned(
-                        child: CircleAvatar(
-                          radius: 26,
-                          backgroundColor: Colors.grey,
-                        ),
+                    ),
+                    Positioned(
+                      child: CircleAvatar(
+                        radius: 26,
+                        backgroundColor: Colors.grey,
                       ),
-                    ],
-                  )),
-                  CircleAvatar(
-                    radius: 26,
-                    backgroundColor: Colors.white,
-                    child: Icon(Icons.search),
-                  )
-                ],
-              ),
+                    ),
+                  ],
+                )),
+                CircleAvatar(
+                  radius: 26,
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.search),
+                )
+              ],
             ),
             Text("List of quites"),
             SizedBox(
