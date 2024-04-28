35c22b46ee0431d292a119b44fad217cdd4bc463
JAICHANGPARK <aristojeff@gmail.com>
2024-04-28 10:36:39 +0900

    Update ep1865_main.dart

---
+++ lib/ep1865_crypto_app/ep1865_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CryptoMobileApp extends StatelessWidget {
+  const CryptoMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
