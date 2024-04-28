fb876ff96d080a14ace20207f0ffe979a76d4839
JAICHANGPARK <aristojeff@gmail.com>
2024-04-28 10:38:37 +0900

    Create crypto_home_page.dart

---
+++ lib/ep1865_crypto_app/view/crypto_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CryptoHomePage extends StatefulWidget {
+  const CryptoHomePage({super.key});
+
+  @override
+  State<CryptoHomePage> createState() => _CryptoHomePageState();
+}
+
+class _CryptoHomePageState extends State<CryptoHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
