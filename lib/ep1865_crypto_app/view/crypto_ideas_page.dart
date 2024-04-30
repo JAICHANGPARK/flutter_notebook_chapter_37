d8263dcbd3dc9d4ded3b83c0ea94d863244e7105
JAICHANGPARK <aristojeff@gmail.com>
2024-04-30 21:57:48 +0900

    Update crypto_ideas_page.dart

--- lib/ep1865_crypto_app/view/crypto_ideas_page.dart
+++ lib/ep1865_crypto_app/view/crypto_ideas_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class CryptoIdeasPage extends StatefulWidget {
   const CryptoIdeasPage({super.key});
@@ -10,6 +11,50 @@ class CryptoIdeasPage extends StatefulWidget {
 class _CryptoIdeasPageState extends State<CryptoIdeasPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 40,
+                          child: CircleAvatar(
+                            radius: 26,
+                            backgroundColor: Colors.white,
+                            child: Icon(Icons.add),
+                          ),
+                        ),
+                        Positioned(
+                          child: CircleAvatar(
+                            radius: 26,
+                            backgroundColor: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )),
+                CircleAvatar(
+                  radius: 26,
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.search),
+                )
+              ],
+            ),
+            Gap(16),
+            Text(
+              "List of quites",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 48,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
