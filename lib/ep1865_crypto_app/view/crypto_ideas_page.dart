f7613d5cc6e5a4a4cc848259023826dd62d51f3b
JAICHANGPARK <aristojeff@gmail.com>
2024-04-30 21:59:29 +0900

    Update crypto_ideas_page.dart

--- lib/ep1865_crypto_app/view/crypto_ideas_page.dart
+++ lib/ep1865_crypto_app/view/crypto_ideas_page.dart
@@ -9,47 +9,92 @@ class CryptoIdeasPage extends StatefulWidget {
 }
 
 class _CryptoIdeasPageState extends State<CryptoIdeasPage> {
+  List<String> tabs = [
+    "All",
+    "Subscriptions",
+    "Editors' Choices",
+    "Subscriptions",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[50],
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                Expanded(
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: 40,
-                          child: CircleAvatar(
-                            radius: 26,
-                            backgroundColor: Colors.white,
-                            child: Icon(Icons.add),
-                          ),
+            Gap(12),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Stack(
+                    children: [
+                      Positioned(
+                        left: 40,
+                        child: CircleAvatar(
+                          radius: 26,
+                          backgroundColor: Colors.white,
+                          child: Icon(Icons.add),
                         ),
-                        Positioned(
-                          child: CircleAvatar(
-                            radius: 26,
-                            backgroundColor: Colors.grey,
-                          ),
+                      ),
+                      Positioned(
+                        child: CircleAvatar(
+                          radius: 26,
+                          backgroundColor: Colors.grey,
                         ),
-                      ],
-                    )),
-                CircleAvatar(
-                  radius: 26,
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.search),
-                )
-              ],
+                      ),
+                    ],
+                  )),
+                  CircleAvatar(
+                    radius: 26,
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.search),
+                  )
+                ],
+              ),
             ),
             Gap(16),
-            Text(
-              "Ideas",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 48,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Text(
+                "Ideas",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 48,
+                ),
+              ),
+            ),
+            Gap(12),
+            SizedBox(
+              height: 36,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: tabs.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: index == 0 ? Colors.black : Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    margin: EdgeInsets.only(right: 16),
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: Center(
+                      child: Text(
+                        "${tabs[index]}",
+                        style: TextStyle(
+                          color: index == 0 ? Colors.white : Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
           ],
