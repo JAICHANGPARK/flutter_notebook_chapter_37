dfe13198fccee36cb3ef1f969c53e6e50580fe4c
JAICHANGPARK <aristojeff@gmail.com>
2024-04-30 21:57:00 +0900

    Update crypto_ideas_page.dart

---
+++ lib/ep1865_crypto_app/view/crypto_ideas_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CryptoIdeasPage extends StatefulWidget {
+  const CryptoIdeasPage({super.key});
+
+  @override
+  State<CryptoIdeasPage> createState() => _CryptoIdeasPageState();
+}
+
+class _CryptoIdeasPageState extends State<CryptoIdeasPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
