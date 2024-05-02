e1fcf6c21df511ec3d89643102d0c6bfbdd3d157
JAICHANGPARK <aristojeff@gmail.com>
2024-05-02 18:50:23 +0900

    Create sneakers_home_page.dart

---
+++ lib/ep1869_sneakers_app/view/sneakers_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SneakersHomePage extends StatefulWidget {
+  const SneakersHomePage({super.key});
+
+  @override
+  State<SneakersHomePage> createState() => _SneakersHomePageState();
+}
+
+class _SneakersHomePageState extends State<SneakersHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
