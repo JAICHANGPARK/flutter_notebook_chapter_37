fe62dcf23e8d21e6e40347d164097960e80bd22e
JAICHANGPARK <aristojeff@gmail.com>
2024-05-02 18:48:30 +0900

    Create 1869_main.dart

---
+++ lib/ep1869_sneakers_app/1869_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SneakersMobileApp extends StatelessWidget {
+  const SneakersMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
