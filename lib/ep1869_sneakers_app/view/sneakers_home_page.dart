2a3aebdedbd7c6306a98e72cf49f7eceeb27ce22
JAICHANGPARK <aristojeff@gmail.com>
2024-05-02 18:52:33 +0900

    Update sneakers_home_page.dart

--- lib/ep1869_sneakers_app/view/sneakers_home_page.dart
+++ lib/ep1869_sneakers_app/view/sneakers_home_page.dart
@@ -12,7 +12,12 @@ class _SneakersHomePageState extends State<SneakersHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        items: [],
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.bookmark_border), label: "bookmark"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.keyboard_command_key), label: "Home"),
+        ],
         showSelectedLabels: false,
         showUnselectedLabels: false,
       ),
