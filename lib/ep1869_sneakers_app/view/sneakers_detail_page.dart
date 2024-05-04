bde43c6fd8229fc06edeafce67ed79c7de2e4c34
JAICHANGPARK <aristojeff@gmail.com>
2024-05-04 10:23:04 +0900

    Update sneakers_detail_page.dart

--- lib/ep1869_sneakers_app/view/sneakers_detail_page.dart
+++ lib/ep1869_sneakers_app/view/sneakers_detail_page.dart
@@ -12,23 +12,31 @@ class _SneakersDetailPageState extends State<SneakersDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Icon(Icons.arrow_back),
-                Text("New Orda"),
-                Spacer(),
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Colors.grey[200]!,
-                  )),
-                  child: Icon(Icons.square_outlined),
-                )
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.arrow_back),
+                  Text("New Orda"),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.grey[200]!,
+                    )),
+                    child: Icon(Icons.square_outlined),
+                  )
+                ],
+              ),
+              Container(
+                height: 400,
+                color: Colors.blue,
+              )
+            ],
+          ),
         ),
       ),
     );
