88261628acb7258b37ad409bc1c3564743d03b7a
JAICHANGPARK <aristojeff@gmail.com>
2024-05-06 08:43:10 +0900

    Update ep1873_main.dart

--- lib/ep1873_manga_app/ep1873_main.dart
+++ lib/ep1873_manga_app/ep1873_main.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class MangaApp extends StatelessWidget {
+  const MangaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
