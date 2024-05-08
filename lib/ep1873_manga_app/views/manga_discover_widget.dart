73817ce2913486b9289efdc1b812f993cddc55f2
JAICHANGPARK <aristojeff@gmail.com>
2024-05-08 21:54:32 +0900

    Update manga_discover_widget.dart

--- lib/ep1873_manga_app/views/manga_discover_widget.dart
+++ lib/ep1873_manga_app/views/manga_discover_widget.dart
@@ -12,6 +12,7 @@ class _MangaDiscoverWidgetState extends State<MangaDiscoverWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text("ALL BOOKS"),
 
