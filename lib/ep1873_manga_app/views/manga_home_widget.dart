dff148a298b8257058521f4baf72a72b6aea033d
JAICHANGPARK <aristojeff@gmail.com>
2024-05-08 21:49:47 +0900

    Create manga_home_widget.dart

---
+++ lib/ep1873_manga_app/views/manga_home_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MangaHomeWidget extends StatefulWidget {
+  const MangaHomeWidget({super.key});
+
+  @override
+  State<MangaHomeWidget> createState() => _MangaHomeWidgetState();
+}
+
+class _MangaHomeWidgetState extends State<MangaHomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
