30b06cb0eb4043e38e7575e9ac28918bdfb1468a
JAICHANGPARK <aristojeff@gmail.com>
2024-05-08 21:52:21 +0900

    Update manga_home_page.dart

--- lib/ep1873_manga_app/views/manga_home_page.dart
+++ lib/ep1873_manga_app/views/manga_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_notebook_chapter_37/ep1873_manga_app/views/manga_discover_widget.dart';
+import 'package:flutter_notebook_chapter_37/ep1873_manga_app/views/manga_home_widget.dart';
 import 'package:gap/gap.dart';
 
 class MangaHomePage extends StatefulWidget {
@@ -31,202 +33,13 @@ class _MangaHomePageState extends State<MangaHomePage> {
         ],
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                  height: 182,
-                  margin: EdgeInsets.symmetric(horizontal: 12),
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 130,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      Gap(12),
-                      Expanded(
-                          child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Leveling, Vol. 5",
-                            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                          Text(
-                            "Chapter 32",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          Gap(12),
-                          Text(
-                            "Unknown",
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                          Gap(24),
-                          Row(
-                            children: [
-                              Text(
-                                "78%",
-                                style: TextStyle(
-                                  color: Colors.orangeAccent,
-                                ),
-                              ),
-                              Gap(12),
-                              Text(
-                                "20 min left",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Gap(4),
-                          LinearProgressIndicator(
-                            value: .7,
-                            color: Colors.amberAccent,
-                          ),
-                          Gap(12),
-                          Container(
-                            padding: EdgeInsets.symmetric(vertical: 8),
-                            decoration:
-                                BoxDecoration(color: Colors.amberAccent, borderRadius: BorderRadius.circular(5)),
-                            child: Center(
-                              child: Text(
-                                "Continue Reading",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ))
-                    ],
-                  )),
-              Gap(16),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "FOR YOU",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.arrow_forward),
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                height: 220,
-                margin: EdgeInsets.only(
-                  left: 16,
-                ),
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 320,
-                      margin: EdgeInsets.only(right: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.blue,
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
-                          ),
-                          Gap(8),
-                          Text(
-                            "Sample Manga",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Gap(4),
-                          Text(
-                            "Chapter 47",
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Gap(16),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "ALL BOOKS",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.arrow_forward),
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  itemCount: 10,
-                  physics: NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 12,
-                    mainAxisSpacing: 12,
-                    childAspectRatio: 0.8,
-                  ),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    );
-                  },
-                ),
-              )
-            ],
-          ),
-        ),
+        child: IndexedStack(
+          index: 0,
+          children: [
+            MangaHomeWidget(),
+            MangaDiscoverWidget(),
+          ],
+        )
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
