fe594b710276992fcb2ce7462a71b00163ffa468
JAICHANGPARK <aristojeff@gmail.com>
2024-05-09 21:29:34 +0900

    Update manga_discover_widget.dart

--- lib/ep1873_manga_app/views/manga_discover_widget.dart
+++ lib/ep1873_manga_app/views/manga_discover_widget.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class MangaDiscoverWidget extends StatefulWidget {
   const MangaDiscoverWidget({super.key});
@@ -26,15 +28,22 @@ class _MangaDiscoverWidgetState extends State<MangaDiscoverWidget> {
       ),
       body: Column(
         children: [
-          SizedBox(
+          Gap(16),
+          Container(
             height: 52,
+            margin: EdgeInsets.only(left: 16),
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
+              itemCount: 10,
               itemBuilder: (context, index) {
                 return Container(
                   decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(.2),
+                    color: index == 0 ? Colors.orangeAccent : Colors.white.withOpacity(.2),
                     borderRadius: BorderRadius.circular(4),
+                  ),
+                  margin: EdgeInsets.only(right: 12),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16,
                   ),
                   child: Center(
                     child: Text(
@@ -45,6 +54,60 @@ class _MangaDiscoverWidgetState extends State<MangaDiscoverWidget> {
               },
             ),
           ),
+          Gap(16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                Text(
+                  "83 books",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "by Popularity",
+                  style: TextStyle(color: Colors.white),
+                ),
+                Icon(
+                  Icons.keyboard_arrow_down,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(
+                16.0,
+                16,
+                16,
+                0,
+              ),
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 160,
+                    margin: EdgeInsets.only(bottom: 12),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          )
         ],
       ),
     );
