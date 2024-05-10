06ecb5d7247daf0b7cb2b735c416f1e0f4d147b2
JAICHANGPARK <aristojeff@gmail.com>
2024-05-10 17:34:38 +0900

    Update meditation_home_page.dart

--- lib/ep1877_meditation_app/views/meditation_home_page.dart
+++ lib/ep1877_meditation_app/views/meditation_home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:gap/gap.dart';
 
 class MeditationHomePage extends StatefulWidget {
   const MeditationHomePage({super.key});
@@ -16,13 +16,32 @@ class _MeditationHomePageState extends State<MeditationHomePage> {
         children: [
           Container(
             height: 120,
-
-            decoration: BoxDecoration(color: Colors.blue[50],
-
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(16),
-                bottomRight:  Radius.circular(16),
-              )
+            decoration: BoxDecoration(
+                color: Colors.blue[50],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                )),
+            padding: EdgeInsets.only(
+              left: 16,
+              top: 62,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(),
+                Gap(8),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Hi, Dreamwalker"),
+                      Text("Good Afternoon!"),
+                    ],
+                  ),
+                )
+              ],
             ),
           )
         ],
