2d9b2551ed75978e8304f79594b5a088cd9b862c
JAICHANGPARK <aristojeff@gmail.com>
2024-05-10 17:29:06 +0900

    Update ep1877_main.dart

--- lib/ep1877_meditation_app/ep1877_main.dart
+++ lib/ep1877_meditation_app/ep1877_main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+
+}
+
+class MeditationMobileApp extends StatelessWidget {
+  const MeditationMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
 }
