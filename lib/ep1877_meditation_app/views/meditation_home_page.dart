102a073baa96e03b8e4c75ac74f055618cb93d53
JAICHANGPARK <aristojeff@gmail.com>
2024-05-10 17:29:41 +0900

    Create meditation_home_page.dart

---
+++ lib/ep1877_meditation_app/views/meditation_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MeditationHomePage extends StatefulWidget {
+  const MeditationHomePage({super.key});
+
+  @override
+  State<MeditationHomePage> createState() => _MeditationHomePageState();
+}
+
+class _MeditationHomePageState extends State<MeditationHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
