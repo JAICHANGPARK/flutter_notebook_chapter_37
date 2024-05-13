58358d552198413a5aa5d52bd364d4f3218f7501
JAICHANGPARK <aristojeff@gmail.com>
2024-05-13 20:34:37 +0900

    Update meditation_sleep_widget.dart

--- lib/ep1877_meditation_app/views/meditation_sleep_widget.dart
+++ lib/ep1877_meditation_app/views/meditation_sleep_widget.dart
@@ -12,24 +12,38 @@ class _MeditationSleepWidgetState extends State<MeditationSleepWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromRGBO(236, 239, 244, 1),
+        leading: CircleAvatar(),
         centerTitle: true,
         title: Text("Sleep"),
         actions: [
           Container(
             decoration: BoxDecoration(
-              color: Colors.grey,
+              color: Colors.grey[200],
               border: Border.all(
                 color: Colors.white,
               ),
               shape: BoxShape.circle,
             ),
             padding: EdgeInsets.all(12),
-
             child: Icon(
               Icons.search,
             ),
-          )
+          ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+                    Row(
+                      children: [
+                        Text("Featured"),
+                      ],
+                    ),
+
+          ],
+        ),
       ),
     );
   }
