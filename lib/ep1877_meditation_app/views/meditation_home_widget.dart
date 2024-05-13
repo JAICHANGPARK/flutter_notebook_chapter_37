8cc21630f179229c62b928abebe8f4017fb43779
JAICHANGPARK <aristojeff@gmail.com>
2024-05-13 20:39:42 +0900

    Update meditation_home_widget.dart

--- lib/ep1877_meditation_app/views/meditation_home_widget.dart
+++ lib/ep1877_meditation_app/views/meditation_home_widget.dart
@@ -229,54 +229,7 @@ class _MeditationHomeWidgetState extends State<MeditationHomeWidget> {
                     ),
                   ),
                   const Gap(12),
-                  ListView.separated(
-                    padding: EdgeInsets.zero,
-                    shrinkWrap: true,
-                    itemCount: 10,
-                    physics: const NeverScrollableScrollPhysics(),
-                    separatorBuilder: (context, index) => const Divider(),
-                    itemBuilder: (context, index) {
-                      return Row(
-                        children: [
-                          Container(
-                            height: 72,
-                            width: 72,
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                          ),
-                          const Gap(12),
-                          const Expanded(
-                            child: Text(
-                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                              maxLines: 2,
-                            ),
-                          ),
-                          Container(
-                            decoration: ShapeDecoration(
-                              shape: const StadiumBorder(),
-                              color: Colors.grey[100],
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 12,
-                              vertical: 6,
-                            ),
-                            child: const Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(
-                                  Icons.play_circle_outline,
-                                  size: 18,
-                                ),
-                                Text("8 MIN"),
-                              ],
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                  )
+
                 ],
               ),
             ),
