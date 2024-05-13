d624ce7e75b34914e8f2d1a87dd96aa453b9e721
JAICHANGPARK <aristojeff@gmail.com>
2024-05-13 20:39:41 +0900

    Update meditation_list_widget.dart

---
+++ lib/ep1877_meditation_app/views/meditation_list_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MeditationListWidget extends StatelessWidget {
+  const MeditationListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
