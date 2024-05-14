5f9e5cbbf4e101d985a229727f275b5544f1d418
JAICHANGPARK <aristojeff@gmail.com>
2024-05-14 19:01:31 +0900

    Update health_home_page.dart

--- lib/ep1880_medical_health_app/views/health_home_page.dart
+++ lib/ep1880_medical_health_app/views/health_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HealthHomePage extends StatefulWidget {
   const HealthHomePage({super.key});
@@ -11,6 +12,34 @@ class _HealthHomePageState extends State<HealthHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Medical & Health"),
+        actions: [
+          Icon(
+            Icons.info_outline,
+            color: Theme.of(context).primaryColor,
+          ),
+          Gap(12),
+          Text(
+            "Help",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Gap(12),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.apps,
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         // selectedItemColor: Color(0xff7ACE59),
         selectedItemColor: Color.fromRGBO(50, 212, 153, 1),
