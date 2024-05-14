1bbcfac99d3ea3b8c07956e044374f7b7e682934
JAICHANGPARK <aristojeff@gmail.com>
2024-05-14 18:55:27 +0900

    Update health_home_page.dart

--- lib/ep1880_medical_health_app/views/health_home_page.dart
+++ lib/ep1880_medical_health_app/views/health_home_page.dart
@@ -10,6 +10,41 @@ class HealthHomePage extends StatefulWidget {
 class _HealthHomePageState extends State<HealthHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.coronavirus_outlined,
+            ),
+            label: "Virus",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.medication_outlined,
+            ),
+            label: "Doctors",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.receipt_long,
+            ),
+            label: "Doctors",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.monitor_heart_outlined,
+            ),
+            label: "Doctors",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.monitor_heart_outlined,
+            ),
+            label: "Doctors",
+          ),
+        ],
+      ),
+    );
   }
 }
