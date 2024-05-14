6b1dad528cb2320c0add2a61416e14b662e707aa
JAICHANGPARK <aristojeff@gmail.com>
2024-05-14 18:52:21 +0900

    Update health_home_page.dart

--- lib/ep1880_medical_health_app/views/health_home_page.dart
+++ lib/ep1880_medical_health_app/views/health_home_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class HealthHomePage extends StatefulWidget {
+  const HealthHomePage({super.key});
+
+  @override
+  State<HealthHomePage> createState() => _HealthHomePageState();
+}
+
+class _HealthHomePageState extends State<HealthHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
