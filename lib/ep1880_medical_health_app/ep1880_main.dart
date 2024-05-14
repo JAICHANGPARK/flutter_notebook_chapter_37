35da1a63312487aacfc28f332a9dbf8938c4fed5
JAICHANGPARK <aristojeff@gmail.com>
2024-05-14 18:49:14 +0900

    Update ep1880_main.dart

---
+++ lib/ep1880_medical_health_app/ep1880_main.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class MedicalHealthApp extends StatelessWidget {
+  const MedicalHealthApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
