0da65f06e9770ceb18bb65c13f0cf99eb3d6c698
JAICHANGPARK <aristojeff@gmail.com>
2024-05-14 18:58:40 +0900

    Update ep1880_main.dart

--- lib/ep1880_medical_health_app/ep1880_main.dart
+++ lib/ep1880_medical_health_app/ep1880_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'views/health_home_page.dart';
 
@@ -12,6 +13,9 @@ class MedicalHealthApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.dmSans().fontFamily,
+      ),
       home: HealthHomePage(),
     );
   }
