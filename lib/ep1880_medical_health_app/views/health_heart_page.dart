efe65e39089b2b5eb9eb103e4796c8063131759f
JAICHANGPARK <aristojeff@gmail.com>
2024-05-16 18:45:41 +0900

    Update health_heart_page.dart

--- lib/ep1880_medical_health_app/views/health_heart_page.dart
+++ lib/ep1880_medical_health_app/views/health_heart_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class HealthHeartPage extends StatefulWidget {
   const HealthHeartPage({super.key});
 
@@ -12,13 +11,32 @@ class _HealthHeartPageState extends State<HealthHeartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-
+              Row(
+                children: [
+                  Container(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.keyboard_arrow_left_outlined,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: Center(
+                    child: Text(
+                      "Heart",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ))
+                ],
+              )
             ],
           ),
         ),
