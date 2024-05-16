61ad8fa819034e9f01c89f57216cf33b7f48e904
JAICHANGPARK <aristojeff@gmail.com>
2024-05-16 18:42:48 +0900

    Update health_heart_page.dart

--- lib/ep1880_medical_health_app/views/health_heart_page.dart
+++ lib/ep1880_medical_health_app/views/health_heart_page.dart
@@ -11,6 +11,18 @@ class HealthHeartPage extends StatefulWidget {
 class _HealthHeartPageState extends State<HealthHeartPage> {
   @override
   Widget build(BuildContext context) {
-    return
-  } const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
 }
