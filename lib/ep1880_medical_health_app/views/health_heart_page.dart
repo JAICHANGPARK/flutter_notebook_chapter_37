da60a18ae3236315346d566b576aa22ba843acf7
JAICHANGPARK <aristojeff@gmail.com>
2024-05-16 18:41:24 +0900

    Update health_heart_page.dart

---
+++ lib/ep1880_medical_health_app/views/health_heart_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HealthHeartPage extends StatefulWidget {
+  const HealthHeartPage({super.key});
+
+  @override
+  State<HealthHeartPage> createState() => _HealthHeartPageState();
+}
+
+class _HealthHeartPageState extends State<HealthHeartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return
+  } const Placeholder();
+}
