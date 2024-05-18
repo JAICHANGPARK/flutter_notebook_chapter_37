30b74141d0b07fcfc437219776e3149e8797a1bf
JAICHANGPARK <aristojeff@gmail.com>
2024-05-18 10:26:21 +0900

    Update ep1885_main.dart

--- lib/ep1885_ebook_app/ep1885_main.dart
+++ lib/ep1885_ebook_app/ep1885_main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  
+}
 
 class EBookApp extends StatelessWidget {
   const EBookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
