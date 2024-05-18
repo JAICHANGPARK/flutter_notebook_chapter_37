6965e0e68ab2e5cad706398ce8212e0fcfaadb20
JAICHANGPARK <aristojeff@gmail.com>
2024-05-18 10:28:08 +0900

    Update ebook_main_page.dart

---
+++ lib/ep1885_ebook_app/views/ebook_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EbookMainPage extends StatefulWidget {
+  const EbookMainPage({super.key});
+
+  @override
+  State<EbookMainPage> createState() => _EbookMainPageState();
+}
+
+class _EbookMainPageState extends State<EbookMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
