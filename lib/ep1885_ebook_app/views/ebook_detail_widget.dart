2e2b75623ea379615768a6626f950d4511578039
JAICHANGPARK <aristojeff@gmail.com>
2024-05-21 18:35:36 +0900

    Update ebook_detail_widget.dart

--- lib/ep1885_ebook_app/views/ebook_detail_widget.dart
+++ lib/ep1885_ebook_app/views/ebook_detail_widget.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class EbookDetailWidget extends StatelessWidget {
-  const EbookDetailWidget({Key? key}) : super(key: key);
+class EbookDetailPage extends StatefulWidget {
+  const EbookDetailPage({super.key});
 
   @override
+  State<EbookDetailPage> createState() => _EbookDetailPageState();
+}
+
+class _EbookDetailPageState extends State<EbookDetailPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Placeholder();
   }
 }
