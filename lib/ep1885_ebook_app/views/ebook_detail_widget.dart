f9fd0a3b711c23e85c2f03f784843c26655d7039
JAICHANGPARK <aristojeff@gmail.com>
2024-05-21 18:40:05 +0900

    Update ebook_detail_widget.dart

--- lib/ep1885_ebook_app/views/ebook_detail_widget.dart
+++ lib/ep1885_ebook_app/views/ebook_detail_widget.dart
@@ -11,11 +11,21 @@ class _EbookDetailPageState extends State<EbookDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(235, 225, 198, 1),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_back,
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
