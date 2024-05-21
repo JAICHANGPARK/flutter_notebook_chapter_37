70d676fdd9ed005e639671eac16eb4d8c561df8d
JAICHANGPARK <aristojeff@gmail.com>
2024-05-21 18:42:46 +0900

    Update ebook_detail_widget.dart

--- lib/ep1885_ebook_app/views/ebook_detail_widget.dart
+++ lib/ep1885_ebook_app/views/ebook_detail_widget.dart
@@ -12,21 +12,70 @@ class _EbookDetailPageState extends State<EbookDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(235, 225, 198, 1),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.arrow_back,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.arrow_back,
+                    ),
                   ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        'Sample',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.share,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
                 ),
-              ],
-            )
-          ],
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text("A Graphic Guide To Title Title Title Title Title"),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[100],
+                          ),
+                          child: Row(
+                            children: [
+                              
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
