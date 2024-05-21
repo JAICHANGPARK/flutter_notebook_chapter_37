71ee0da5bd53e6154105f1b722186bada2bb7162
JAICHANGPARK <aristojeff@gmail.com>
2024-05-21 18:36:42 +0900

    Update ebook_detail_widget.dart

--- lib/ep1885_ebook_app/views/ebook_detail_widget.dart
+++ lib/ep1885_ebook_app/views/ebook_detail_widget.dart
@@ -10,6 +10,12 @@ class EbookDetailPage extends StatefulWidget {
 class _EbookDetailPageState extends State<EbookDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
