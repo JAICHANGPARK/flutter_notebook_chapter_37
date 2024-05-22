e78967293c928001094ebd48905a0708894f838d
JAICHANGPARK <aristojeff@gmail.com>
2024-05-22 19:43:08 +0900

    Create ep1889_main.dart

---
+++ lib/ep1889_music_app/ep1889_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MusicApp());''
+}
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
