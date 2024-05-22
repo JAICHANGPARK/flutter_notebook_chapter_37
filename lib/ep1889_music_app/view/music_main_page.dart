153d68226cefd566cd5c737dcd8805e6e4eaacf4
JAICHANGPARK <aristojeff@gmail.com>
2024-05-22 19:44:31 +0900

    Create music_main_page.dart

---
+++ lib/ep1889_music_app/view/music_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MusicMainPage extends StatefulWidget {
+  const MusicMainPage({super.key});
+
+  @override
+  State<MusicMainPage> createState() => _MusicMainPageState();
+}
+
+class _MusicMainPageState extends State<MusicMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
