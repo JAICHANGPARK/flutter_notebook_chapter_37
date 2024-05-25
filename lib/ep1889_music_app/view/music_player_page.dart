4f9f32e8d2d3658fc5d5c98f6caa1e9cddfc1969
JAICHANGPARK <aristojeff@gmail.com>
2024-05-25 10:02:26 +0900

    Create music_player_page.dart

---
+++ lib/ep1889_music_app/view/music_player_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MusicPlayerPage extends StatefulWidget {
+  const MusicPlayerPage({super.key});
+
+  @override
+  State<MusicPlayerPage> createState() => _MusicPlayerPageState();
+}
+
+class _MusicPlayerPageState extends State<MusicPlayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
