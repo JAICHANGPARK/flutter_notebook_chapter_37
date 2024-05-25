ab677d60910ee031e126c5df7d0250834d109cce
JAICHANGPARK <aristojeff@gmail.com>
2024-05-25 10:02:59 +0900

    Update music_player_page.dart

--- lib/ep1889_music_app/view/music_player_page.dart
+++ lib/ep1889_music_app/view/music_player_page.dart
@@ -11,6 +11,6 @@ class MusicPlayerPage extends StatefulWidget {
 class _MusicPlayerPageState extends State<MusicPlayerPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
