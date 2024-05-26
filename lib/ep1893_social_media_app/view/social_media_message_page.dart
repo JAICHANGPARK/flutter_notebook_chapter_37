319f2c74f95f4c5acc0170032fb9e7ec66f4ffa7
JAICHANGPARK <aristojeff@gmail.com>
2024-05-26 11:13:02 +0900

    Update social_media_message_page.dart

--- lib/ep1893_social_media_app/view/social_media_message_page.dart
+++ lib/ep1893_social_media_app/view/social_media_message_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class SocialMediaMessagePage extends StatefulWidget {
+  const SocialMediaMessagePage({super.key});
+
+  @override
+  State<SocialMediaMessagePage> createState() => _SocialMediaMessagePageState();
+}
+
+class _SocialMediaMessagePageState extends State<SocialMediaMessagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
