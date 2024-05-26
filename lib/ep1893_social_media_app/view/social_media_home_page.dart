6b349d801f16fbffafb73f383b7c1a2ad9462a8f
JAICHANGPARK <aristojeff@gmail.com>
2024-05-26 11:05:31 +0900

    Create social_media_home_page.dart

---
+++ lib/ep1893_social_media_app/view/social_media_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SocialMediaHomePage extends StatefulWidget {
+  const SocialMediaHomePage({super.key});
+
+  @override
+  State<SocialMediaHomePage> createState() => _SocialMediaHomePageState();
+}
+
+class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
