8a348ee23dbba558c176a7dc119128592ae8bf30
JAICHANGPARK <aristojeff@gmail.com>
2024-05-26 11:03:43 +0900

    Update ep1893_main.dart

--- lib/ep1893_social_media_app/ep1893_main.dart
+++ lib/ep1893_social_media_app/ep1893_main.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+
+class SocialMediaApp extends StatelessWidget {
+  const SocialMediaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
