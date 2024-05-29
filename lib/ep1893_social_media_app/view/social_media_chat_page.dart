23d33df6a8e4a91b0d4a3b6e96cb96f1f3961328
JAICHANGPARK <aristojeff@gmail.com>
2024-05-29 19:20:12 +0900

    Update social_media_chat_page.dart

---
+++ lib/ep1893_social_media_app/view/social_media_chat_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaChatPage extends StatefulWidget {
+  const SocialMediaChatPage({super.key});
+
+  @override
+  State<SocialMediaChatPage> createState() => _SocialMediaChatPageState();
+}
+
+class _SocialMediaChatPageState extends State<SocialMediaChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(),
+                Container(
+                  width: 32,
+                  height: 32,
+                  
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
