3924e253062dfb9d5705d3526b994ad8eaa2e2f6
JAICHANGPARK <aristojeff@gmail.com>
2024-05-30 18:56:35 +0900

    Create education_start_page.dart

---
+++ lib/ep1897_education_app/views/education_start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EducationStartPage extends StatefulWidget {
+  const EducationStartPage({super.key});
+
+  @override
+  State<EducationStartPage> createState() => _EducationStartPageState();
+}
+
+class _EducationStartPageState extends State<EducationStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
