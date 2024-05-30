a92733949acb7c2b1ae90fa290c96554f21d8b06
JAICHANGPARK <aristojeff@gmail.com>
2024-05-30 18:55:59 +0900

    Create eeo1897_main.dart

---
+++ lib/ep1897_education_app/eeo1897_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class EducationApp extends StatelessWidget {
+  const EducationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
