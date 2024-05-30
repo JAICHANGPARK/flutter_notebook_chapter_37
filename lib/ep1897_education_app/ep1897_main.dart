1f5f211ae071729716b12c18d940c0740d05aaf6
JAICHANGPARK <aristojeff@gmail.com>
2024-05-30 18:57:21 +0900

    Update ep1897_main.dart

--- lib/ep1897_education_app/ep1897_main.dart
+++ lib/ep1897_education_app/ep1897_main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  
+}
 
 class EducationApp extends StatelessWidget {
   const EducationApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: EducationStartPage(),
+    );
   }
 }
