2d4d548e988ac69a5a8a9ac4474d377624b24cd1
JAICHANGPARK <aristojeff@gmail.com>
2024-05-30 19:01:31 +0900

    Update education_start_page.dart

--- lib/ep1897_education_app/views/education_start_page.dart
+++ lib/ep1897_education_app/views/education_start_page.dart
@@ -18,7 +18,20 @@ class _EducationStartPageState extends State<EducationStartPage> {
             fit: BoxFit.cover,
           ),
         ),
-        
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          top: 0,
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Colors.black,
+                Colors.black.withOpacity(.4),
+              ]),
+            ),
+          ),
+        )
       ],
     );
   }
