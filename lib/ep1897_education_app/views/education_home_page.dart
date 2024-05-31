c626fa7a1356454d7b2e5b5c41021f3ea4fee2bf
JAICHANGPARK <aristojeff@gmail.com>
2024-05-31 18:34:59 +0900

    Update education_home_page.dart

--- lib/ep1897_education_app/views/education_home_page.dart
+++ lib/ep1897_education_app/views/education_home_page.dart
@@ -15,42 +15,52 @@ class _EducationHomePageState extends State<EducationHomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                CircleAvatar(),
-                Expanded(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Dreamwalker",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 28,
                       ),
-                    ),
-                    Text(
-                      "Unknown",
-                      style: TextStyle(
-                        color: Colors.grey,
+                      Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Dreamwalker",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "Unknown",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ],
+                          )),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.search,
+                        ),
                       ),
-                    )
-                  ],
-                )),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.search,
-                  ),
-                ),
-                Badge(
-                  child: Icon(
-                    Icons.notifications_active_outlined,
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            )
+                      Badge(
+                        child: Icon(
+                          Icons.notifications_active_outlined,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+
           ],
         ),
       ),
